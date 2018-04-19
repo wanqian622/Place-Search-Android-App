ba68ade34552999ff20c115aa34cc100978cbae5
wanqian622 <wanqian622@gmail.com>
2018-04-18 20:53:34 -0700

    modify map xml

--- app/src/main/java/com/example/wanqianhw9/MapFragment.java
+++ app/src/main/java/com/example/wanqianhw9/MapFragment.java
@@ -3,15 +3,23 @@ package com.example.wanqianhw9;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MapFragment extends Fragment {
+    private EditText mGeolocationEditText;
+    private Spinner spinner;
+    private View mView;
 
 
     public MapFragment() {
@@ -22,8 +30,30 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        mView = inflater.inflate(R.layout.fragment_map, container, false);
+        mGeolocationEditText = (EditText) mView.findViewById(R.id.editTextGeolocation);
+        spinner = (Spinner) mView.findViewById(R.id.travel_model_spinner);
+        String defaultValue = "Driving";
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.travel_mode_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        int spinnerPosition = adapter.getPosition(defaultValue);
+        spinner.setSelection(spinnerPosition);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        return mView;
     }
 
 }
